3f0c0de1c8edfb9b89068a7a8462f25f8c41a0d5
rajendra <rajendrammarboyana@gmail.com>
2024-03-26 07:16:35 +0000

    create framework for all toos can be created. added prometheus to be created. created pipeline to run terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 module "tools" {
-  for_each =  var.tools
-  source = "./modules"
-  tool_name = each.key
-  instance_type = each.value["instance_type"]
+  for_each = var.tools
+  source   = "./module"
+
+  tool_name            = each.key
+  instance_type        = each.value["instance_type"]
+
   zone_id = var.zone_id
 }
